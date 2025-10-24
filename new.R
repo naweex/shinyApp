102323cc9e0335d9ac3244eb5a23b5954c9d5968
Navid Bahrami <142748078+naweex@users.noreply.github.com>
2025-10-24 12:12:32 +0330

    Delete new.R

--- new.R
+++
@@ -1,7 +0,0 @@
-    cars = c(1, 3, 6, 4, 9) # nolint
-    trucks = c(2, 5, 4, 5, 12)
-    plot(cars, type="o", col="blue", ylim=c(0,12))
-
-    lines(trucks, type="o", pch=22, lty=2, col="red") # nolint
-
-    title(main="Autos", col.main="red", font.main=4) # nolint
